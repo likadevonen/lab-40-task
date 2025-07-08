dbff88658c78670a0507e308415b74bf8623dba8
Iliya Devedzhiev <i.devedjiev@gmail.com>
2025-07-08 16:10:27 +0300

    chore: tiny retouch of welcom route

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 
 Route::get('/', function () {
-    return View::make('welcome');
-});
-
-Route::get('/support-and-maintenance', function () {
-    return View::make('support-and-maintenance', ['name' => 'Iliya Devedzhiev']);
+    return View::make('support-and-maintenance');
 });
